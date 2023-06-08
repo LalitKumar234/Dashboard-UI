ea8822de129fa900e24d7038057fe1293ce1c6b9
lalit sahu <lsahu0838@gmail.com>
2023-06-08 22:31:53 +0530

    Some changes

--- src/axiosService.js
+++ src/axiosService.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const baseURL = `http://localhost:3200`
+// const baseURL = `http://localhost:3200`
+const baseURL = `https://dashboard-ui-new.onrender.com`
 
 export const axiosGet = async (route) => {
     return await axios.get(`${baseURL}/${route}`)
